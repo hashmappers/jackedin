e70b125934f41f1d3743a7a25cca415134dc66a7
Rene Unrau <runrau@esri.com>
2018-07-22 11:35:52 -0700

    clean up

--- src/data/app.ts
+++ src/data/app.ts
@@ -1,28 +1,48 @@
-import StreamLayer from "esri/layers/StreamLayer";
 import EsriMap from "esri/Map";
+import StreamLayer from "esri/layers/StreamLayer";
 
 const calculateScore = (target: any) => {
-  // target.graphic.attributes;
-  // target.graphic.geometry;
-  return "\"This is my first tweet 3esri\" has a score of 0.15"
+  const tfidfScore = (Math.random() * (1 - 0)).toFixed(3);
+  const locScore = (Math.random() * (1 - 0)).toFixed(3);
+  return `{polarity} This tweet has a TFIDF Score of ${tfidfScore} and a location score of ${locScore}`;
+}
+
+const createUniqueValueInfos = () => {
+  const fireflyImages = [
+    "cat1.png",
+    "cat2.png",
+    "cat3.png",
+    "cat4.png",
+    "cat5.png"
+  ];
+
+  const baseUrl =
+    "https://arcgis.github.io/arcgis-samples-javascript/sample-data/";
+
+  return fireflyImages.map(function(url, i) {
+    return {
+      value: (i + 1) * 0.2,
+      symbol: {
+        type: "picture-marker",
+        url: baseUrl + url
+      }
+    }
+  });
 }
 
 const streamLayer = new StreamLayer({
-  //url: "https://ps0002022.esri.com/server/rest/services/twitter-out-connector-2/StreamServer",
-  url: "https://geoeventsample1.esri.com:6443/arcgis/rest/services/LABus/StreamServer",
+  url: "https://ps0002022.esri.com/server/rest/services/stream-service-out-original2/StreamServer",
   purgeOptions: {
     displayCount: 1000
   },
   popupTemplate: {
-    title: "{seconds_since_report}",
+    title: "{text}",
     content: calculateScore
   },
   renderer: {
-    type: "simple",
-    symbol: {
-      type: "picture-marker",
-      url: "https://arcgis.github.io/arcgis-samples-javascript/sample-data/cat5.png"
-    }
+    type: "unique-value",
+    field: "polarity",
+    uniqueValueInfos: createUniqueValueInfos()
   }
 });
 
